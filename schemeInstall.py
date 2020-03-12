d7d2f0c8508644cd62d9591eb8bb6963185d2c14
Sobieg <sobiegrus@gmail.com>
2020-03-12 17:18:37 +0300

    add file output support

--- schemeInstall.py
+++ schemeInstall.py
@@ -18,6 +18,9 @@ parser.add_argument("BITS",
                     default=1024,
                     help="Number of bits, default = 1024")
 
+parser.add_argument("-f",
+                    help="file to save")
+
 args = parser.parse_args()
 
 exp = 0x10001
@@ -27,8 +30,19 @@ pubkey, privkey = rsa.newkeys(args.BITS, exponent=exp)
 # print(pubkey)
 # print(privkey)
 
-print("exp = \"" + str(hex(exp)[2:]) + "\"")
-print("n = \"" + str(hex(pubkey.n))[2:] + "\"")
-print("d = \"" + str(hex(privkey.d))[2:] + "\"")
-print("p = \"" + str(hex(privkey.p))[2:] + "\"")
-print("q = \"" + str(hex(privkey.q))[2:] + "\"")
+try:
+    mystr = "exp = \"" + str(hex(exp)[2:]) + "\"" + "\n" +\
+            "n = \"" + str(hex(pubkey.n))[2:] + "\"" + "\n" + \
+            "d = \"" + str(hex(privkey.d))[2:] + "\"" + "\n"\
+            "p = \"" + str(hex(privkey.p))[2:] + "\"" + "\n"\
+            "q = \"" + str(hex(privkey.q))[2:] + "\""
+    with open(args.f, "w") as file:
+        file.write(mystr)
+except:
+    print("exp = \"" + str(hex(exp)[2:]) + "\"")
+    print("n = \"" + str(hex(pubkey.n))[2:] + "\"")
+    print("d = \"" + str(hex(privkey.d))[2:] + "\"")
+    print("p = \"" + str(hex(privkey.p))[2:] + "\"")
+    print("q = \"" + str(hex(privkey.q))[2:] + "\"")
+
+
