33bf71f0433a55dcfea1df7022eb65fb4e6efb00
Sobieg <sobiegrus@gmail.com>
2020-03-13 18:01:43 +0300

    done woth schemeinstall.py ????

--- schemeinstall.py
+++ schemeinstall.py
@@ -1,6 +1,7 @@
 import argparse
 import rsa  # for keys
-
+from sympy import nextprime, gcd, randprime, is_primitive_root
+from random import randint
 
 
 def auto_int(x):
@@ -11,20 +12,22 @@ def get_args():
     parser = argparse.ArgumentParser(description='RSA encoder and digital signature generator')
 
     parser.add_argument("BITS",
-                    nargs="?",
-                    type=auto_int,
-                    default=1024,
-                    help="Number of bits, default = 1024")
+                        nargs="?",
+                        type=auto_int,
+                        default=1024,
+                        help="Number of bits, default = 1024")
 
     parser.add_argument("SCH",
                         nargs="?",
                         default="RSA",
+                        type=str,
                         help="Scheme for wich generete params, RSA|ELG")
 
     parser.add_argument("-f",
-                    help="file to save")
+                        help="file to save")
 
     return parser.parse_args()
+
 
 def gen_rsa(filename, bits):
     exp = 0x10001
@@ -32,15 +35,13 @@ def gen_rsa(filename, bits):
     pubkey, privkey = rsa.newkeys(bits, exponent=exp)
     signpubkey, signprivkey = rsa.newkeys(1024, exponent=exp)
 
-
-
     try:
-        mystr = "exp = \"" + str(hex(exp)[2:]) + "\"" + "\n"\
-                "n = \"" + str(hex(pubkey.n))[2:] + "\"" + "\n"\
-                "d = \"" + str(hex(privkey.d))[2:] + "\"" + "\n"\
-                "p = \"" + str(hex(privkey.p))[2:] + "\"" + "\n"\
-                "q = \"" + str(hex(privkey.q))[2:] + "\"" + "\n"\
-                "sign_n = \"" + str(hex(signpubkey.n))[2:] + "\"" + "\n"\
+        mystr = "exp = \"" + str(hex(exp)[2:]) + "\"" + "\n" \
+                "n = \"" + str(hex(pubkey.n))[2:] + "\"" + "\n" \
+                "d = \"" + str(hex(privkey.d))[2:] + "\"" + "\n" \
+                "p = \"" + str(hex(privkey.p))[2:] + "\"" + "\n" \
+                "q = \"" + str(hex(privkey.q))[2:] + "\"" + "\n" \
+                "sign_n = \"" + str(hex(signpubkey.n))[2:] + "\"" + "\n" \
                 "sign_d = \"" + str(hex(signprivkey.d))[2:] + "\""
         with open(filename, "w") as file:
             file.write(mystr)
@@ -52,17 +53,55 @@ def gen_rsa(filename, bits):
         print("q = \"" + str(hex(privkey.q))[2:] + "\"")
 
 
-def gen_elg(filename):
+def gen_elg(filename, bits):
+    def gen_p():
+        return randprime(2**bits, 2**(bits+1))
 
-    pass
+    def gen_a(p):
+        def gen_primitive_root(r):
+            a = randint(2**bits, 2**(bits+1))
+            phi_r = r-1 #r is prime
+            while pow(a, phi_r, r) != 1: #aways true, as r is prime
+                a = randint(2 ** bits, 2 ** bits)
+
+            return a
+
+        r = randprime(2**(bits-1), p)
+        return r, gen_primitive_root(r)
+
+    def gen_x(r):
+        return randprime(1, r) #Maybe x should be big?
+
+    def gen_b(a, x, p):
+        return pow(a, x, p)
+
+
+    p = gen_p()
+    r, a = gen_a(p)
+    x = gen_x(r)
+    b = gen_b(a, x, p)
+
+    try:
+        mystr = "p = \"" + str(hex(p)[2:]) + "\"" + "\n" \
+        "a = \"" + str(hex(a))[2:] + "\"" + "\n" \
+        "x = \"" + str(hex(x))[2:] + "\"" + "\n" \
+        "b = \"" + str(hex(b))[2:] + "\"" + "\n" \
+        "r = \"" + str(hex(r))[2:] + "\"" + "\n"
+        with open(filename, "w") as file:
+            file.write(mystr)
+    except:
+        print("p= \"" + str(hex(p)[2:]) + "\"")
+        print("a = \"" + str(hex(a))[2:] + "\"")
+        print("x = \"" + str(hex(x))[2:] + "\"")
+        print("b = \"" + str(hex(b))[2:] + "\"")
+        print("r = \"" + str(hex(r))[2:] + "\"")
+
 
 if __name__ == '__main__':
     args = get_args()
 
-    if args.SHC == "RSA":
-        gen_rsa(args.f)
+    if args.SCH == "RSA":
+        gen_rsa(args.f, args.BITS)
 
-    elif args.SHC == "ELG":
-        gen_elg(args.f)
-
-
+    elif args.SCH == "ELG":
+        gen_elg(args.f, args.BITS)
