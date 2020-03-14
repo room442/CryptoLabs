50df892b90f2dd0f24f4f23902264112333e99c2
Sobieg <sobiegrus@gmail.com>
2020-03-14 18:00:10 +0300

    client done?

---
+++ mo-client.py
@@ -0,0 +1,36 @@
+import asyncio
+from crypto import MOencrypt, MOdecrypt, AES256encrypt
+from random import randint
+from asn import MOencodeFinish, MOdecodeFinish, MOencodeMessage, MOdecodeMessage, MOencodeParams, MOdecodeParams, MOencodeResponse, MOdecodeResponse
+
+
+async def tcp_client(p, encrypted, key, a, loop, len):
+    reader, writer = await asyncio.open_connection('127.0.0.1', 8888,
+                                                   loop=loop)
+
+    writer.write(MOencodeParams(p, p-1, MOencrypt(int.from_bytes(key, "big"), a, p)))
+
+    # t_ab = MOdecodeResponse(await reader.read(100))
+
+    # writer.write(MOencodeFinish(MOdecrypt(t_ab, a, p), len, encrypted)
+
+    writer.close()
+
+
+def sendfile(filename, p):
+    a = randint(2, p-1)
+    with open(filename, "rb") as file:
+        data = file.read()
+
+    datalen = len(data)
+    encrypted, key = AES256encrypt(data)
+
+    loop = asyncio.get_event_loop()
+
+    loop.run_until_complete(tcp_client(p, encrypted, key, a, loop, datalen))
+    loop.close()
+
+
+if __name__ == '__main__':
+    from params import *
+    sendfile("opentext.txt", int(r, 16))
