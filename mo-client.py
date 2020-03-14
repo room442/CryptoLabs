519b923537f4abdb4e917e145955e3bb6b149048
Sobieg <sobiegrus@gmail.com>
2020-03-14 18:01:46 +0300

    ip, port for debugging

--- mo-client.py
+++ mo-client.py
@@ -4,8 +4,8 @@ from random import randint
 from asn import MOencodeFinish, MOdecodeFinish, MOencodeMessage, MOdecodeMessage, MOencodeParams, MOdecodeParams, MOencodeResponse, MOdecodeResponse
 
 
-async def tcp_client(p, encrypted, key, a, loop, len):
-    reader, writer = await asyncio.open_connection('127.0.0.1', 8888,
+async def tcp_client(ip, port, p, encrypted, key, a, loop, len):
+    reader, writer = await asyncio.open_connection(ip, port,
                                                    loop=loop)
 
     writer.write(MOencodeParams(p, p-1, MOencrypt(int.from_bytes(key, "big"), a, p)))
@@ -17,7 +17,7 @@ async def tcp_client(p, encrypted, key, a, loop, len):
     writer.close()
 
 
-def sendfile(filename, p):
+def sendfile(filename, p, ip, port):
     a = randint(2, p-1)
     with open(filename, "rb") as file:
         data = file.read()
@@ -27,10 +27,10 @@ def sendfile(filename, p):
 
     loop = asyncio.get_event_loop()
 
-    loop.run_until_complete(tcp_client(p, encrypted, key, a, loop, datalen))
+    loop.run_until_complete(tcp_client(ip, port, p, encrypted, key, a, loop, datalen))
     loop.close()
 
 
 if __name__ == '__main__':
     from params import *
-    sendfile("opentext.txt", int(r, 16))
+    sendfile("opentext.txt", int(r, 16), "127.0.0.1", 8888)
