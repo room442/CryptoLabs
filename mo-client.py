149a20a95e5d499255ed76ed493958d6c811d62f
Sobieg <sobiegrus@gmail.com>
2020-03-14 22:51:41 +0300

    rework to syncronius client-server on sockets, there is a bug, but idk how to fix it yet

--- mo-client.py
+++ mo-client.py
@@ -3,34 +3,41 @@ from crypto import MOencrypt, MOdecrypt, AES256encrypt
 from random import randint
 from asn import MOencodeFinish, MOencodeParams, MOdecodeResponse
 
+import socket
 
-async def tcp_client(ip, port, p, encrypted, key, a, loop, len):
-    reader, writer = await asyncio.open_connection(ip, port,
-                                                   loop=loop)
 
-    writer.write(MOencodeParams(p, p-1, MOencrypt(int.from_bytes(key, "big"), a, p)))
-
-    t_ab = MOdecodeResponse(await reader.read())
-
-    writer.write(MOencodeFinish(MOdecrypt(t_ab, a, p), len, encrypted))
-
-    writer.close()
+# async def tcp_client(ip, port, p, encrypted, key, a, loop, len):
+#     reader, writer = await asyncio.open_connection(ip, port,
+#                                                    loop=loop)
+#
+#     writer.write(MOencodeParams(p, p-1, MOencrypt(int.from_bytes(key, "big"), a, p)))
+#
+#     t_ab = MOdecodeResponse(await reader.read())
+#
+#     writer.write(MOencodeFinish(MOdecrypt(t_ab, a, p), len, encrypted))
+#
+#     writer.close()
 
 
 def sendfile(filename, p, ip, port):
-    a = randint(2, p-1)
+    a = randint(2, p - 1)
     with open(filename, "rb") as file:
         data = file.read()
 
     datalen = len(data)
     encrypted, key = AES256encrypt(data)
 
-    loop = asyncio.get_event_loop()
+    print(key)
 
-    loop.run_until_complete(tcp_client(ip, port, p, encrypted, key, a, loop, datalen))
-    loop.close()
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect((ip, port))
+
+    client.send(MOencodeParams(p, p-1, MOencrypt(int.from_bytes(key, "big"), a, p)))
+    t_ab = MOdecodeResponse(client.recv(1024))
+    client.send(MOencodeFinish(MOdecrypt(t_ab, a, p), datalen, encrypted))
 
 
 if __name__ == '__main__':
     from params import *
+
     sendfile("opentext.txt", int(r, 16), "127.0.0.1", 8888)
