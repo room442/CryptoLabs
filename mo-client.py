e0b0bee3a7918980e50b5d3c524a0487cb80cee5
Sobieg <sobiegrus@gmail.com>
2020-03-18 12:58:52 +0300

    Rewrite to syncronius server-client, some bugfixes, but still in testing

--- mo-client.py
+++ mo-client.py
@@ -2,25 +2,27 @@ import asyncio
 from crypto import MOencrypt, MOdecrypt, AES256encrypt
 from random import randint
 from asn import MOencodeFinish, MOencodeParams, MOdecodeResponse
+from sympy import randprime
 
 import socket
 
 
-# async def tcp_client(ip, port, p, encrypted, key, a, loop, len):
-#     reader, writer = await asyncio.open_connection(ip, port,
-#                                                    loop=loop)
-#
-#     writer.write(MOencodeParams(p, p-1, MOencrypt(int.from_bytes(key, "big"), a, p)))
-#
-#     t_ab = MOdecodeResponse(await reader.read())
-#
-#     writer.write(MOencodeFinish(MOdecrypt(t_ab, a, p), len, encrypted))
-#
-#     writer.close()
 
+
+def get_data(connection):
+    data = b""
+    while True:
+        newpart = connection.recv(1024)
+        if not newpart:
+            break
+        else:
+            data = data + newpart
+        if len(data) < 1024:
+            break
+    return data
 
 def sendfile(filename, p, ip, port):
-    a = randint(2, p - 1)
+    a = randprime(2, p - 1)
     with open(filename, "rb") as file:
         data = file.read()
 
@@ -32,9 +34,17 @@ def sendfile(filename, p, ip, port):
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect((ip, port))
 
-    client.send(MOencodeParams(p, p-1, MOencrypt(int.from_bytes(key, "big"), a, p)))
-    t_ab = MOdecodeResponse(client.recv(1024))
-    client.send(MOencodeFinish(MOdecrypt(t_ab, a, p), datalen, encrypted))
+    integer_key = int.from_bytes(key, "big")
+    t_a = MOencrypt(integer_key, a, p)
+    client.send(MOencodeParams(p, p-1, t_a))
+
+    msg = get_data(client)
+    t_ab = MOdecodeResponse(msg)
+    t_b = MOdecrypt(t_ab, a, p)
+
+    client.send(MOencodeFinish(t_b, datalen, encrypted))
+
+    print("debug")
 
 
 if __name__ == '__main__':
