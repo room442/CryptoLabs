90daa16645ff9de6e0f5176d4484d862adbb4f08
Sobieg <sobiegrus@gmail.com>
2020-03-28 11:36:07 +0300

    fix spacing

--- util.py
+++ util.py
@@ -4,11 +4,12 @@ import math
 def auto_int(x):
     return int(x, 0)
 
+
 def egcd(a, b):
     if a == 0:
         return (b, 0, 1)
     else:
-        g, y, x = egcd(b%a, a)
+        g, y, x = egcd(b % a, a)
         return g, x - (b // a) * y, y
 
 
@@ -17,7 +18,8 @@ def modinv(a, m):
     if g != 1:
         raise Exception("modular inverse does not exist")
     else:
-        return x%m
+        return x % m
+
 
 def contFrac(x, k=3):
     cf = []
