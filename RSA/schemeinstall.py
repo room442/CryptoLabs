76eaf7a2b36415006896a65cadf29eab5897848a
Sobieg <sobiegrus@gmail.com>
2020-03-28 17:53:18 +0300

    add wienere-vuln generation

--- RSA/schemeinstall.py
+++ RSA/schemeinstall.py
@@ -1,6 +1,9 @@
 import argparse
 import rsa  # for keys
-from util import auto_int
+from util import auto_int, modinv
+from sympy import randprime
+from random import randint
+from math import sqrt
 
 
 def get_args():
@@ -34,11 +37,11 @@ def gen_rsa(filename, bits):
 
     try:
         mystr = F"exp = \"{hex(exp)[2:]} \"\n" \
-                F"n = \"{hex(pubkey.n)[2:]}\"\n"\
-                F"d = \"{hex(privkey.d)[2:]}\"\n"\
-                F"p = \"{hex(privkey.p)[2:]}\"\n"\
-                F"q = \"{hex(privkey.q)[2:]}\"\n"\
-                F"sign_n = \"{hex(signpubkey.n)[2:]}\"\n"\
+                F"n = \"{hex(pubkey.n)[2:]}\"\n" \
+                F"d = \"{hex(privkey.d)[2:]}\"\n" \
+                F"p = \"{hex(privkey.p)[2:]}\"\n" \
+                F"q = \"{hex(privkey.q)[2:]}\"\n" \
+                F"sign_n = \"{hex(signpubkey.n)[2:]}\"\n" \
                 F"sign_d = \"{hex(signprivkey.d)[2:]} \""
         with open(filename, "w") as file:
             file.write(mystr)
@@ -48,6 +51,30 @@ def gen_rsa(filename, bits):
         print(F"d = \"{hex(privkey.d)[2:]}\"")
         print(F"p = \"{hex(privkey.p)[2:]}\"")
         print(F"q = \"{hex(privkey.q)[2:]}\"")
+
+
+def gen_rsa_wiener_vuln(filename, bits):
+    p = randprime(2 ** (bits - 1), 2 ** bits)
+    q = randprime(2 ** (bits - 1), 2 ** bits)
+    n = p * q
+    d = randint(0x10001, (1 / 3) * pow(n, 0.25))
+    e = modinv(d, (p - 1) * (q - 1))
+    try:
+        mystr = F"exp = \"{hex(e)[2:]} \"\n" \
+                F"n = \"{hex(n)[2:]}\"\n" \
+                F"d = \"{hex(d)[2:]}\"\n" \
+                F"p = \"{hex(p)[2:]}\"\n" \
+                F"q = \"{hex(q)[2:]}\"\n" \
+                F"sign_n = \"{hex(n)[2:]}\"\n" \
+                F"sign_d = \"{hex(d)[2:]} \""
+        with open(filename, "w") as file:
+            file.write(mystr)
+    except:
+        print(F"exp = \"{hex(e)[2:]}\"")
+        print(F"n = \"{hex(n)[2:]}\"")
+        print(F"d = \"{hex(d)[2:]}\"")
+        print(F"p = \"{hex(p)[2:]}\"")
+        print(F"q = \"{hex(q)[2:]}\"")
 
 
 if __name__ == '__main__':
