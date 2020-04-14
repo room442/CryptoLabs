7518f312281b487f9097cee129ab7f7139770e2e
Sobieg <sobiegrus@gmail.com>
2020-04-14 10:48:27 +0300

    add comments to cipher

--- cipher.py
+++ cipher.py
@@ -6,7 +6,7 @@ import sys
 import asn1
 
 
-def parse(decoder, integers):
+def parse(decoder, integers): # Функция разбора asn.1 файла, которая просто достает оттуда все числовые значения.
     while not decoder.eof():
         try:
             tag = decoder.peek()
@@ -14,21 +14,21 @@ def parse(decoder, integers):
             if tag.nr == asn1.Numbers.Null:
                 break
 
-            if tag.typ == asn1.Types.Primitive:
+            if tag.typ == asn1.Types.Primitive: #если тип примитивный
                 tag, value = decoder.read()
 
-                if tag.nr == asn1.Numbers.Integer:
-                    integers.append(value)
+                if tag.nr == asn1.Numbers.Integer: #если тип Integer
+                    integers.append(value) #добавить к возвращаемому массиву
 
             else:
-                decoder.enter()
+                decoder.enter() #если тип не примитивный, входим для разбора
                 integers = parse(decoder, integers)
                 decoder.leave()
 
         except asn1.Error:
             break
 
-    return integers
+    return integers #возвращаем все целочисленные значения
 
 
 def RSAencrypt(m, e, n):
@@ -40,14 +40,14 @@ def RSAdecrypt(c, d, n):
 
 
 # iv = get_random_bytes(AES.block_size)
-iv = b'\x00' * AES.block_size
+iv = b'\x00' * AES.block_size #синхропосылка нулевая, чтобы не хранить ее
 
 
 def aes_encrypt(data):
-    key = get_random_bytes(AES.key_size[-1])
+    key = get_random_bytes(AES.key_size[-1]) #ключ рандомный
     cipher = AES.new(key, AES.MODE_CBC, iv)
 
-    ciphertext = cipher.encrypt(pad(data, AES.block_size))
+    ciphertext = cipher.encrypt(pad(data, AES.block_size)) #зашифровали. Pad -- дополнение до полного блока.
 
     return ciphertext, key
 
@@ -55,7 +55,7 @@ def aes_encrypt(data):
 def aes_decrypt(data, key):
     decipher = AES.new(key, AES.MODE_CBC, iv)
 
-    decrypted = unpad(decipher.decrypt(data), AES.block_size)
+    decrypted = unpad(decipher.decrypt(data), AES.block_size) #расшифровали и отбросили дополнение
 
     return decrypted
 
@@ -70,49 +70,48 @@ def to_asn(
     encoder = asn1.Encoder()
     encoder.start()
 
-    encoder.enter(asn1.Numbers.Sequence)  # Start of sequence_1 -- main sequence of metadata
-    encoder.enter(asn1.Numbers.Set)  # Start of set_1 -- set of RSA keys
-    encoder.enter(asn1.Numbers.Sequence)  # Start of sequence_2 -- first RSA key
+    encoder.enter(asn1.Numbers.Sequence)  # Основная последовательность
+    encoder.enter(asn1.Numbers.Set)  # Набор ключей RSA
+    encoder.enter(asn1.Numbers.Sequence)  # Последовательность -- первый ключ RSA
 
-    encoder.write(b'\x00\x01', asn1.Numbers.OctetString)  # RSA identifier
-    encoder.write(b'\x0C\x00', asn1.Numbers.UTF8String)  # ID of the key (empty)
+    encoder.write(b'\x00\x01', asn1.Numbers.OctetString)  # идентификатор RSA
+    encoder.write(b'\x0C\x00', asn1.Numbers.UTF8String)  # Необзяталеьный идентификатор ключа
 
-    encoder.enter(asn1.Numbers.Sequence)  # Start of sequence_3 -- sequence of e and n of RSA
+    encoder.enter(asn1.Numbers.Sequence)  # последовательность, содержащая n, e -- открытый ключ
     encoder.write(n, asn1.Numbers.Integer)
     encoder.write(e, asn1.Numbers.Integer)
-    encoder.leave()  # End of sequence_3
+    encoder.leave()  # Вышли из последовательности открытого ключа
 
-    encoder.enter(
-        asn1.Numbers.Sequence)  # Cryptographic parameters, not used in RSA, because parameter n is a part of open key
+    encoder.enter(asn1.Numbers.Sequence)  # Параметры криптосистемы, в RSA не используются
     encoder.leave()
 
-    encoder.enter(asn1.Numbers.Sequence)  # Start of sequence_4 -- sequence of RSA encrypted data
+    encoder.enter(asn1.Numbers.Sequence)  # Зашифрованные данные RSA
     encoder.write(c, asn1.Numbers.Integer)
+    encoder.leave() # Вышли из зашифрованных данных RSA
+
+    encoder.leave()  # Вышли из множества первого ключа RSA
+    encoder.leave()  # Вышли из набора ключей
+
+    encoder.enter(asn1.Numbers.Sequence)  # Последовательность дополнительных данных
+    encoder.write(b'\x10\x82', asn1.Numbers.OctetString)  # идентификатор алгоритма шифрования AES CBC
+    encoder.write(len, asn1.Numbers.Integer)  # Длина шифровтекста
     encoder.leave()
 
-    encoder.leave()  # End of sequence_2
-    encoder.leave()  # End of set_1
+    encoder.leave()  # Вышли из основной последовательности.
 
-    encoder.enter(asn1.Numbers.Sequence)  # Start of sequence_5 -- sequence of additional data
-    encoder.write(b'\x10\x82', asn1.Numbers.OctetString)  # AES CBC identifier
-    encoder.write(len, asn1.Numbers.Integer)  # len of the ciphertext
-    encoder.leave()
+    encoder.write(encrypted) # Записали зашифрованные данные (зашифрованы при помощи AES CBC)
 
-    encoder.leave()  # End of sequence_1
-
-    encoder.write(encrypted)
-
-    return encoder.output()
+    return encoder.output() # вывод
 
 
 def from_asn(filename):  # type: (filename) -> (n, e, c, ciphertext)
-    integers = []  # list of integers in ASN.1 file
+    integers = []
     with open(filename, "rb") as file:
         data = file.read()
         decoder = asn1.Decoder()
         decoder.start(data)
-        integers = parse(decoder, integers)
-        cipher = data[-integers[-1]:]  # get last integrs[last] symbols, this is ciphertext
+        integers = parse(decoder, integers) # получаем все целочисленные значения
+        cipher = data[-integers[-1]:]  # мы знаем длину шифротекста и он в самом конце данных, так что можем просто взять последние байты
     return integers[0], integers[1], integers[2], cipher
 
 
