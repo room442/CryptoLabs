e32efca6879266e7e3f3c048e563478539a4f929
Evgeniy <sobiegrus@gmail.com>
2020-05-01 17:53:06 +0300

    add curves.py scretch

---
+++ GOST/curves.py
@@ -0,0 +1,12 @@
+from util import modinv
+
+def point_double(x, y, p, A):
+    k = ((3*x1**2 + A)*modinv(2*y1, p))%p
+    x3 = (k**2 - 2*x1)%p
+    y3 =
+
+def point_add(x1, y1, x2, y2, p, A):
+    if
+    k = ((3*x1**2 + A)*modinv(2*y1, p))%p
+    x3 = (k**2 - 2*x1)%p
+    y3 =
