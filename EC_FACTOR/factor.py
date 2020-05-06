236455e9b80e2df928480371cd37cb5f55e9589d
Evgeniy <sobiegrus@gmail.com>
2020-05-06 14:55:09 +0300

    file reformat

--- EC_FACTOR/factor.py
+++ EC_FACTOR/factor.py
@@ -1,13 +1,13 @@
 import time
-from sympy import nextprime, gcd # you can implement this function by yours hand and miller-rabin algo
+from sympy import nextprime, gcd  # you can implement this function by yours hand and miller-rabin algo
 from params import n
 import argparse
 from random import randint
 from math import log2
 import curves_common as crv
-from util import modinv
 
 N = int(n, 16)
+
 
 def parse_args():
     parser = argparse.ArgumentParser(description='factorization tool')
@@ -16,6 +16,7 @@ def parse_args():
                         default=10000,
                         help="Number of primes for factorization (Default = 10000)")
     return parser.parse_args()
+
 
 def get_primes(n):
     primes = []
@@ -26,10 +27,10 @@ def get_primes(n):
         p = 0
         for line in f:
             primes.append(int(line))
-            p = p+1
+            p = p + 1
             if p >= n:
                 break
-        print(F"Close file, time = {time.time()-start}")
+        print(F"Close file, time = {time.time() - start}")
 
     while len(primes) < n:
         primes.append(nextprime(primes[-1]))
@@ -41,26 +42,23 @@ def factor(n, primes):
     iter = 1
     while True:
         print(".", end="")
-        if iter%100 == 0:
+        if iter % 100 == 0:
             print()
-        Qx, Qy = randint(1, n-1), randint(1, n-1) # we can choose random curve, so there is no need for choosing curve
-                                                  # and finding point, we can generate point and the curve
-        A = randint(1, n-1)
-        B = (Qy*Qy - Qx * Qx * Qx - A * Qx) % n
+        Qx, Qy = randint(1, n - 1), randint(1,n - 1)  # we can choose random curve, so there is no need for choosing curve
+                                                      # and finding point, we can generate point and the curve
+        A = randint(1, n - 1)
+        B = (Qy * Qy - Qx * Qx * Qx - A * Qx) % n
         i = 0
         Qix, Qiy = Qx, Qy
         try:
             for p in primes:
-                ai = int(0.5 * log2(n)//log2(p))
+                ai = int(0.5 * log2(n) // log2(p))
                 for j in range(ai):
                     Qix, Qiy = crv.point_mult(Qix, Qiy, p, n, A)
         except Exception as e:
             _, g = e.args
             return g
-        iter = iter+1
-
-
-
+        iter = iter + 1
 
 
 if __name__ == '__main__':
@@ -68,5 +66,4 @@ if __name__ == '__main__':
     start = time.time()
     p = factor(N, get_primes(args.m))
     end = time.time()
-    print(F"\nFound p: {hex(p)}, q: {hex(N//p)}, time: {end-start}s")
-
+    print(F"\nFound p: {hex(p)}, q: {hex(N // p)}, time: {end - start}s")
