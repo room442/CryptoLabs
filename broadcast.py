f338715563ecc014838ab9f91a4c2576a0c9921b
Stasya <starikowaanastasia@gmail.com>
2020-05-08 12:03:15 +0300

    remove unused imports, and comments

--- broadcast.py
+++ broadcast.py
@@ -1,8 +1,6 @@
 from broadcast_params import *
 from sympy.ntheory.modular import crt
 from decimal import Decimal
-from aes_common import AES
-from Crypto.Random import get_random_bytes
 
 
 def RSAencrypt(m, e, n):
@@ -27,13 +25,7 @@ def attack(ms):
 if __name__ == '__main__':
 
     ms = []
-    #FIXME: не работает на больших числах (порядка 256 бит)
-
-
-    # sec = get_random_bytes(AES.key_size[-1])
-    # print(F"sec: {sec}")
     for i, _ in enumerate(n):
-        # ms.append(RSAencrypt(int.from_bytes(sec, "big"), e, int(n[i], 16)))
         ms.append(RSAencrypt(0x123456789, e, int(n[i], 16)))
 
     attack(ms)
